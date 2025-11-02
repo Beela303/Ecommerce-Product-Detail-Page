8b98f3d34e91976d65d83e6ab5e4f00f788a44f8
Beela303 <devbeela@gmail.com>
2025-11-02 22:14:36 +0100

    Site Nav component

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="site-nav">
+    <ul>
+      <li>Homepage<span class="material-symbols-outlined">keyboard_arrow_right</span></li>
+      <li>Women<span class="material-symbols-outlined">keyboard_arrow_right</span></li>
+      <li>Women's Shirts & Tops<span class="material-symbols-outlined">keyboard_arrow_right</span></li>
+      <li>Long Sleeve Overshirt, Khaki, 6</li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#site-nav {
+  margin: 33px 100px;
+
+  ul {
+    display: flex;
+
+    li {
+      color: #8F8F8F;
+      font-size: 16px;
+
+      span {
+        margin: 7px;
+        font-size: 12px;
+      }
+
+      &:last-child {
+        color: #292929;
+        margin: 4px;
+      }
+    }
+  }
+}
+</style>
