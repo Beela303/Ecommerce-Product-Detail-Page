da24cee39a923f6fe365eb3f0a80f41d3ec55bd3
Beela303 <devbeela@gmail.com>
2025-11-02 22:13:37 +0100

    Menu

---
+++ src/components/MenuItems.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="menu">
+    <img src="../assets/logo-john-lewis.png" alt="Logo" />
+
+    <ul>
+      <li><span class="material-symbols-outlined">search</span></li>
+      <li>Categories<span class="material-symbols-outlined">keyboard_arrow_down</span></li>
+      <li><span class="material-symbols-outlined">person</span>Sign In</li>
+      <li><span class="material-symbols-outlined">favorite</span></li>
+      <li><span class="material-symbols-outlined">shopping_basket</span></li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#menu {
+  color: #141414;
+
+  height: 94.03px;
+  margin: 0 100px;
+  padding: 31px;
+
+  border-bottom: 0.5px dotted #a3a3a3;
+
+  display: flex;
+  justify-content: space-between;
+
+  img {
+    width: 170px;
+    height: 32.03px;
+
+    //padding: 31px 0;
+  }
+
+  ul {
+    display: flex;
+
+    li {
+      margin-right: 20px;
+      font-size: 20px;
+
+      display: flex;
+
+      cursor: pointer;
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+}
+</style>
