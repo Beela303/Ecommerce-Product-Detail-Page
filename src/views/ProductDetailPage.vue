52c41e72d628b2d308d54e6934012f986ec38d77
Beela303 <devbeela@gmail.com>
2025-11-03 03:22:46 +0100

    included components

--- src/views/ProductDetailPage.vue
+++ src/views/ProductDetailPage.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import DiscountInfo from '@/components/DiscountInfo.vue';
+import MenuItems from '@/components/MenuItems.vue';
+import SiteNavigation from '@/components/SiteNavigation.vue';
+import ProductDescription from '@/components/ProductDescription.vue';
+import RelatedProduct from '@/components/RelatedProduct.vue';
+
 </script>
 
 <template>
   <DiscountInfo />
+  <MenuItems />
+  <SiteNavigation />
+  <ProductDescription />
+  <RelatedProduct />
+
 </template>
