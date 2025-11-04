9e7453e3efd218266f593379f899f26b178a8c66
Beela303 <devbeela@gmail.com>
2025-11-04 23:19:28 +0100

    color variables

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,29 @@
 
 <style lang="scss">
 :root {
-  --background-color: #C599B6;
+  --ORANGE: #FFA439;
+  --TEXT-BLUE: #3E3E59;
+
+  --BORDER01: #B8B8B8;
+  --BORDER02: #A3A3A3;
+  --BORDER03: #333333;
+
+  --BLACK01: #0B0F0E;
+  --BLACK02: #292929;
+  --BLACK03: #141414;
+  --BLACK04: #121212;
+
+  --GRAY01: #E4E9EE;
+  --GRAY02: #525252;
+  --GRAY03: #818B9C;
+  --GRAY04: #C4C8CC;
+  --GRAY05: #7A7A7A;
+  --GRAY06: #F2F2F2;
+  --GRAY07: #787A7C;
+
+  --WHITE01: #FFF;
+  --WHITE02: #EBEBEB;
+  --WHITE03: #E6E6E6;
 }
 
 * {
@@ -25,6 +47,10 @@
 
 body {
   overflow-x: hidden;
+}
+
+a {
+  text-decoration: none;
 }
 
 button {
