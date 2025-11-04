a9114986239488de81a71966f4b7b907279db931
Beela303 <devbeela@gmail.com>
2025-11-05 00:23:19 +0100

    updated api

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,20 +1,19 @@
-<!--import axios, { AxiosResponse } from 'axios'
+//import axios, { AxiosResponse } from 'axios'
 
-interface User {
-  id: number
-  name: string
-  email: string
-}
+//interface User {
+//  id: number
+//  name: string
+//  email: string
+//}
 
-const apiClient = axios.create({
-  baseURL: 'https://frontendcodingtest-production.up.railway.app/api/products/{1}',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
+//const apiClient = axios.create({
+//  baseURL: 'https://frontendcodingtest-production.up.railway.app/api/products/{1}',
+//  headers: {
+//    'Content-Type': 'application/json',
+//  },
+//})
 
-export const fetchUsers = async (): Promise<User[]> => {
-  const response: AxiosResponse<User[]> = await apiClient.get('/product')
-  return response.data
-}
--->
+//export const fetchUsers = async (): Promise<User[]> => {
+//  const response: AxiosResponse<User[]> = await apiClient.get('/product')
+//  return response.data
+//}
